f003ea905387b570fd7e040759f62047cee34ad9
sleepingsaint <suryasantosh14523@gmail.com>
2020-10-18 15:43:03 +0530

    Updates

---
+++ precache-manifest.d2b020fd186961d2ac9b815e28689b7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32d96fb6f4235e43ce6036e092ed0c0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af1ec9ef06722f3cdc9a",
+    "url": "/static/css/2.b63eaa2c.chunk.css"
+  },
+  {
+    "revision": "f96cab9b6195d687b6db",
+    "url": "/static/css/main.d5c37166.chunk.css"
+  },
+  {
+    "revision": "af1ec9ef06722f3cdc9a",
+    "url": "/static/js/2.ad93d2cc.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.ad93d2cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f96cab9b6195d687b6db",
+    "url": "/static/js/main.3f8ed1d1.chunk.js"
+  },
+  {
+    "revision": "296faae12aa8969e6769",
+    "url": "/static/js/runtime-main.f272ed14.js"
+  }
+]);
